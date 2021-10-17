a04878f0705f32d184150332463ed3d134c13fd3
Alex Waller <alex@artofalexwaller.com>
2021-10-16 22:03:47 -0400

    switch statement

--- practice.js
+++ practice.js
@@ -481,6 +481,17 @@ if (crashBandicoot[1] == 'Cortex'){
     const number2 = frogNo[0] > 10 ? 'red' : 'blue';
     console.log(number2);
     // evaluated to false so 'blue' is printed to console
+    switch(number2) {
+        case 'red':
+            console.log('color is red');
+            break;
+        case 'blue':
+            console.log('color is blue');
+            break;
+        default:
+            console.log('color is not red or blue');
+            break;
+    }
 }
 
 
