a96455f0efc9cc53c1a66156ff603b808bd62dc8
Alex Waller <alex@artofalexwaller.com>
2021-10-16 23:43:04 -0400

    CrashAndCortex

---
+++ object.js
@@ -0,0 +1,64 @@
+console.log("Greetings, Mars.");
+
+const robots = ['Hank-44', 'Warren-21', 'Mellon-Tech',
+'Eggplant-Head'];
+
+console.log(robots);
+
+const hammerHead = ['Hammerhead', 'Geometry Man', 'Taylor', 
+'Harvey', 'Wibaux', 'The Cosmic Trout'];
+
+console.log('----------------------------------------------');
+console.log(hammerHead);
+
+const crashBandicoot = ['Crash Bandicoot', 'Cortex', 'Ripper Roo',
+'Spyro', 'Dingodile', 'Tiny Tiger', 'Crunch Bandicoot', 'N. Ginn'];
+
+console.log('----------------------------------------------');
+console.log(crashBandicoot);
+
+const troutNo = [37, 44, 29, 87, 56, 11, 34, 45];
+
+console.log('----------------------------------------------');
+console.log(troutNo);
+
+const frogNo = [1, 2, 4, 5, 9, 12, 16, 21, 24, 25, 36];
+
+console.log('----------------------------------------------');
+console.log(frogNo);
+
+const bandicoot = {
+    firstName: 'Crash',
+    lastName: 'Bandicoot',
+    species:'Bandicoot',
+    interests: 'Nitro Cart Racing',
+    personality: 'Highly Enthusiastic',
+    rival: 'Doctor Neo Cortex',
+    placeOfBirth: 'Outback, Australia',
+    carts: {
+        cart1: 'The Crash Wagon',
+        cart2: 'The Cortex Warp Cart'
+    },
+    friends: ['Spyro', 'Coco', 'Shifty', 'King Chicken']
+}
+
+console.log('-----------------------------------------------');
+console.log(bandicoot);
+
+const cortex = {
+    firstName: 'Neo',
+    lastName: 'Cortex',
+    species: 'Human',
+    interests: 'Cutting Edge Science',
+    personality: 'Devious and Enigmatic',
+    rival: 'Crash Bandicoot',
+    placeOfBirth: 'Peoria, Illinois',
+    carts:{
+        cart1: 'Oxide Cruiser',
+        cart2: 'Dingo-Mobile'
+    },
+    friends: ['N. Ginn', 'Dingodile', 'Tiny Tiger', 'Nitrous Oxide']
+}
+
+console.log('-----------------------------------------------------');
+console.log(cortex);
