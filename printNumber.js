a571d9474c01d3caae2e2cd43a07858e6d0740cf
Alex Waller <alex@artofalexwaller.com>
2021-10-20 16:39:58 -0400

    querySelectorAll

--- printNumber.js
+++ printNumber.js
@@ -5,3 +5,13 @@ console.log(window);
 
 const numberForm = document.getElementById("numbers-form");
 console.log(numberForm);
+// prints HTML of form to the console
+const numberForm2 = document.querySelector("numbers-form");
+console.log(numberForm2);
+// returned null
+console.log(document.querySelector("h1"));
+// prints <h1> HTML to the console
+
+// multiple element
+console.log(document.querySelectorAll('.number'));
+// NodeList(3) [li.number, li.number, li.number]
