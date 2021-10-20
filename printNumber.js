5d74dd71f8d45f9ac2af42156f9928b899aed518
Alex Waller <alex@artofalexwaller.com>
2021-10-20 17:14:19 -0400

    addEventListener

--- printNumber.js
+++ printNumber.js
@@ -24,3 +24,24 @@ const numbers1 = document.querySelectorAll('.number');
 
 numbers1.forEach((number) => console.log(number));
 // prints out each HTML element of the number class to the console
+
+const ul = document.querySelector('.numbers');
+// ul.remove();
+// will remove that HTML element from the DOM
+
+// ul.lastElementChild.remove();
+// will remove last HTML element of that class
+
+// ul.firstElementChild.textContent = 'Hi, Bodhi!';
+// will change the text of the slected HTML element
+
+// ul.children[1].innerText = 'Hi, Chipper!';
+// will change the text of the selected HTML element
+
+// ul.lastElementChild.innerHTML = '<h1>Hi, Beezer!</h1>';
+// will change the content and HTML element of the selected element
+
+const button1 = document.getElementById('numbers-button');
+button1.style.background = 'green';
+// turns the button green
+button1.addEventListener('click');
