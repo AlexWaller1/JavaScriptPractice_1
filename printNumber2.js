485578e362ae4116fec1e3e52522d31325175860
Alex Waller <alex@artofalexwaller.com>
2021-10-20 22:56:53 -0400

    numberListPrint

--- printNumber2.js
+++ printNumber2.js
@@ -1,5 +1,29 @@
 const numbersForm = document.querySelector('#numbers-form');
-const numberInput1 = document.querySelector('#numbers');
+const numberInput1 = document.querySelector('#number');
+const msg = document.querySelector('.msg');
+const numberList = document.querySelector('#numbers');
 
 console.log(numbersForm);
-console.log(numberInput1);
+
+
+numbersForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    console.log(numberInput1.value);
+
+    if (numberInput1 === '') {
+        msg.innerHTML = 'Please Fill Out Form.';
+    }
+    else {
+        console.log('success');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`
+        The number is ${numberInput1.value}`));
+        numberList.appendChild(li);
+
+        // clear fields
+        numberInput1.value = '';
+    }
+       
+}
